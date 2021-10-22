a212316f03924e48f9fdec85fac32c356693be78
mtfrat <martinfraticelli@gmail.com>
2021-10-22 19:43:28 -0300

    desafio clase 4

---
+++ src/components/ComponenteContainer/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+const ItemListContainer = () => {
+    return (
+        <div>
+            <h1 className="mt-5">Soy el componente container</h1>
+        </div>
+    )
+}
+
+export default ItemListContainer
